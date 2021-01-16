a725d624524f3c3ff0c810da0eb3a721af56d2d8
Nihal Ismail <nihalismailk@gmail.com>
2021-01-16 19:06:52 +0300

    10% Homescreen

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -30,12 +30,14 @@ class _HomePageState extends State<HomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 IconButton(
+                  padding: EdgeInsets.zero,
                   icon: Icon(Icons.keyboard_arrow_left),
                   onPressed: (){
 
                   },
                 ),
                 IconButton(
+                  padding: EdgeInsets.zero,
                   icon: Icon(Icons.crop_square_rounded),
                   onPressed: (){
 
@@ -43,35 +45,125 @@ class _HomePageState extends State<HomePage> {
                 )
               ],
             ),
+
             Row(
               children: [
-                Text("Choose"),
-                SizedBox(width: ScreenUtil().setWidth(16),),
-                Text("Device")
+                Text(
+                  "Choose",
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 26,
+                    fontWeight: FontWeight.w100
+                  ),
+                ),
+                SizedBox(width: ScreenUtil().setWidth(8),),
+                Text(
+                  "Sneakers",
+                  style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 26,
+                      fontWeight: FontWeight.w800
+                  ),
+                ),
               ],
             ),
+            SizedBox(
+              height: ScreenUtil().setHeight(32),
+            ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Container(
-                  color: Colors.black,
-                  height: 20,
-                  width: 20,
+                Row(
+                  children: [
+                    Container(
+                      height: ScreenUtil().setHeight(64),
+                      width: ScreenUtil().setWidth(64),
+                      child: Center(
+                        child: Icon(Icons.search),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              offset: Offset(0,4),
+                              blurRadius: 4
+                          ),
+                          BoxShadow(
+                              color: Colors.black12,
+                              offset: Offset(0,-6),
+                              blurRadius: 4
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: ScreenUtil().setWidth(16),
+                    ),
+                    Container(
+                      height: ScreenUtil().setHeight(64),
+                      width: ScreenUtil().setWidth(64),
+                      child: Center(
+                        child: Icon(Icons.filter_alt_outlined),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              offset: Offset(0,4),
+                              blurRadius: 4
+                          ),
+                          BoxShadow(
+                              color: Colors.black12,
+                              offset: Offset(0,-6),
+                              blurRadius: 4
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: ScreenUtil().setWidth(16),
+                    ),
+                  ],
                 ),
                 Container(
-                  color: Colors.black,
-                  height: 20,
-                  width: 20,
-                ),
-                Container(
-                  color: Colors.black,
-                  height: 20,
-                  width: 60,
+                  height: ScreenUtil().setHeight(64),
+                  width: ScreenUtil().setWidth(148),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: ScreenUtil().setWidth(16)
+                  ),
+                  child: Row(
+                    children: [
+                      Text("Cart",style: TextStyle(color: Colors.white),),
+                      SizedBox(width: ScreenUtil().setWidth(16),),
+                      Text("XX",style: TextStyle(color: Colors.white),),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
                 ),
               ],
+            ),
+            SizedBox(
+              height: ScreenUtil().setHeight(32),
             ),
             Expanded(
-              child: Container(
-                color: Colors.red,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(color: Colors.red,),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(color: Colors.yellow,),
+                  )
+                ],
               ),
             )
           ],
