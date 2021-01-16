5db8d721b6110a9de1bcdcc662f62539ee7a1b96
Nihal Ismail <nihalismailk@gmail.com>
2021-01-16 21:00:05 +0300

    Product page 20%

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -26,150 +26,152 @@ class _HomePageState extends State<HomePage> {
     var provider = Provider.of<ShoeProvider>(context);
     List<Shoe> _shoes = provider.shoes;
 
-    return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: ScreenUtil().setWidth(32)
-        ),
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [
-            SizedBox(
-              height: ScreenUtil().setHeight(24),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  icon: Icon(Icons.keyboard_arrow_left),
-                  onPressed: (){
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: ScreenUtil().setWidth(32)
+          ),
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            children: [
+              SizedBox(
+                height: ScreenUtil().setHeight(24),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    icon: Icon(Icons.keyboard_arrow_left),
+                    onPressed: (){
 
-                  },
-                ),
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  icon: Icon(Icons.crop_square_rounded),
-                  onPressed: (){
-
-                  },
-                )
-              ],
-            ),
-
-            Row(
-              children: [
-                Text(
-                  "Choose",
-                  style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 26,
-                    fontWeight: FontWeight.w100
+                    },
                   ),
-                ),
-                SizedBox(width: ScreenUtil().setWidth(8),),
-                Text(
-                  "Sneakers",
-                  style: TextStyle(
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    icon: Icon(Icons.crop_square_rounded),
+                    onPressed: (){
+
+                    },
+                  )
+                ],
+              ),
+
+              Row(
+                children: [
+                  Text(
+                    "Choose",
+                    style: TextStyle(
                       color: Colors.black87,
                       fontSize: 26,
-                      fontWeight: FontWeight.w800
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: ScreenUtil().setHeight(32),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      height: ScreenUtil().setHeight(64),
-                      width: ScreenUtil().setWidth(64),
-                      child: MaterialButton(
-                        color: Colors.white,
-                        elevation: 8,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15)
-                        ),
-                        onPressed: (){},
-                        child: Center(
-                          child: Icon(Icons.search),
-                        ),
-                      ),
-
-                    ),
-                    SizedBox(
-                      width: ScreenUtil().setWidth(16),
-                    ),
-                    Container(
-                      height: ScreenUtil().setHeight(64),
-                      width: ScreenUtil().setWidth(64),
-                      child: MaterialButton(
-                        color: Colors.white,
-                        elevation: 8,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15)
-                        ),
-                        onPressed: (){},
-                        child: Center(
-                          child: Icon(Icons.filter_alt_outlined),
-                        ),
-                      ),
-
-                    ),
-                    SizedBox(
-                      width: ScreenUtil().setWidth(16),
-                    ),
-                  ],
-                ),
-                Container(
-                  height: ScreenUtil().setHeight(64),
-                  width: ScreenUtil().setWidth(148),
-                  child: MaterialButton(
-                    color: Colors.black,
-                    elevation: 8,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15)
-                    ),
-                    onPressed: (){},
-                    child: Center(
-                      child: Row(
-                        children: [
-                          Text("Cart    XXX",style: TextStyle(color: Colors.white),)
-                        ],
-                      )
+                      fontWeight: FontWeight.w100
                     ),
                   ),
-
-                ),
-              ],
-            ),
-            SizedBox(
-              height: ScreenUtil().setHeight(32),
-            ),
-            Expanded(
-              child: WaterfallFlow.builder(
-                itemCount: _shoes.length,
-                padding: EdgeInsets.zero,
-                gridDelegate: SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: ScreenUtil().setHeight(30),
-                  crossAxisSpacing: ScreenUtil().setWidth(30),
-                ),
-                itemBuilder: (ctx,index){
-                  if(index==1)
-                    return ShoeWidget(shoe: _shoes[index], hasMargin: true);
-                  else
-                    return ShoeWidget(shoe: _shoes[index], hasMargin: false);
-                },
+                  SizedBox(width: ScreenUtil().setWidth(8),),
+                  Text(
+                    "Sneakers",
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 26,
+                        fontWeight: FontWeight.w800
+                    ),
+                  ),
+                ],
               ),
-            )
-          ],
+              SizedBox(
+                height: ScreenUtil().setHeight(32),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: ScreenUtil().setHeight(64),
+                        width: ScreenUtil().setWidth(64),
+                        child: MaterialButton(
+                          color: Colors.white,
+                          elevation: 8,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15)
+                          ),
+                          onPressed: (){},
+                          child: Center(
+                            child: Icon(Icons.search),
+                          ),
+                        ),
+
+                      ),
+                      SizedBox(
+                        width: ScreenUtil().setWidth(16),
+                      ),
+                      Container(
+                        height: ScreenUtil().setHeight(64),
+                        width: ScreenUtil().setWidth(64),
+                        child: MaterialButton(
+                          color: Colors.white,
+                          elevation: 8,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)
+                          ),
+                          onPressed: (){},
+                          child: Center(
+                            child: Icon(Icons.filter_alt_outlined),
+                          ),
+                        ),
+
+                      ),
+                      SizedBox(
+                        width: ScreenUtil().setWidth(16),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: ScreenUtil().setHeight(64),
+                    width: ScreenUtil().setWidth(148),
+                    child: MaterialButton(
+                      color: Colors.black,
+                      elevation: 8,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                      onPressed: (){},
+                      child: Center(
+                        child: Row(
+                          children: [
+                            Text("Cart    XXX",style: TextStyle(color: Colors.white),)
+                          ],
+                        )
+                      ),
+                    ),
+
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: ScreenUtil().setHeight(32),
+              ),
+              Expanded(
+                child: WaterfallFlow.builder(
+                  itemCount: _shoes.length,
+                  padding: EdgeInsets.zero,
+                  gridDelegate: SliverWaterfallFlowDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: ScreenUtil().setHeight(30),
+                    crossAxisSpacing: ScreenUtil().setWidth(30),
+                  ),
+                  itemBuilder: (ctx,index){
+                    if(index==1)
+                      return ShoeWidget(shoe: _shoes[index], hasMargin: true);
+                    else
+                      return ShoeWidget(shoe: _shoes[index], hasMargin: false);
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -1,6 +1,7 @@
 import 'package:design_sneakers/models/shoe.dart';
 import 'package:design_sneakers/providers/shoes_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
 class ProductPage extends StatefulWidget {
@@ -15,13 +16,91 @@ class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
 
+    //Screen Util Init
+    ScreenUtil.init(context, designSize: Size(414, 896), allowFontScaling: true);
+
+    //Provider data
     final provider = Provider.of<ShoeProvider>(context,listen: false);
     Shoe shoe = provider.selectedShoe;
 
-    return Scaffold(
-      body: Center(
-        child: Hero(tag: shoe.name,child: Image.asset(provider.selectedShoe.imageURL)),
-      )
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          alignment: Alignment.bottomCenter,
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.black,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    bottom: ScreenUtil().setHeight(32)
+                  ),
+                  child: Text("Buy Now",style: TextStyle(color: Colors.white),),
+                ),
+              ),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(75),
+                bottomLeft:  Radius.circular(75)
+              ),
+              child: Container(
+                color: Colors.white,
+                width: double.infinity,
+                height: double.infinity,
+                margin: EdgeInsets.only(
+                    bottom: ScreenUtil().setHeight(120)
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: ScreenUtil().setHeight(24),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.keyboard_arrow_left),
+                          onPressed: (){
+
+                          },
+                        ),
+                        IconButton(
+                          icon: provider.isShoeInWishlist(shoe)? Icon(Icons.favorite):Icon(Icons.favorite_border),
+                          onPressed: (){
+                            setState(() {
+                              provider.addToWishList(shoe);
+                            });
+                          },
+                        )
+                      ],
+                    ),
+                    Hero(
+                        tag: shoe.name,
+                        child: Image.asset(
+                          shoe.imageURL,
+                          height: ScreenUtil().setHeight(414),
+                          width: ScreenUtil().setWidth(414),)
+                    ),
+                    Row(),
+                    SingleChildScrollView(
+                      child: Column(
+                        children: [
+
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        )
+      ),
     );
   }
 }
